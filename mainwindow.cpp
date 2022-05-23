f96ec5cc4cd79e5e329445f05d759906514ce21e
Peck Timothy <timothy.peck@he-arc.ch>
2022-05-23 14:25:18 +0200

    Remove surplus styles

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,9 +15,6 @@ MainWindow::MainWindow(QWidget *parent)
     output = new QLabel("Resultat");
     button = new QPushButton("Chercher");
 
-    addStyles();
-
-    connect(input, &QLineEdit::textChanged, this, &MainWindow::updateText);
     connect(button, &QPushButton::clicked, this, &MainWindow::find);
 
     vLayout->addWidget(input);
@@ -37,34 +34,6 @@ MainWindow::MainWindow(QWidget *parent)
  */
 void MainWindow::find(){
     //Call find func
-}
-
-/**
- * @brief MainWindow::updateText
- * @param text
- */
-void MainWindow::updateText(const QString& text){
-    // call the calculate function, and put the output in "output"
-    output->setText(text);
-}
-
-void MainWindow::addStyles(){
-    QString stylesLineEdit="background-color: #f1f1f1";
-    QString stylesLabel="background-color: #f1f1f1;";
-    QString stylesButton="border-radius: 5px;"
-                         "background-color: lightblue;"
-                         "min-width: 90px;"
-                         "width: 50%;"
-                         "min-height: 20px;"
-                         "border-style: outset;"
-                         "border-width: 2px;"
-                         "border-color: white;";
-    QString stylesSelf="background-color: white";
-
-    input->setStyleSheet(stylesLineEdit);
-    output->setStyleSheet(stylesLabel);
-    button->setStyleSheet(stylesButton);
-    setStyleSheet(stylesSelf);
 }
 
 /**
